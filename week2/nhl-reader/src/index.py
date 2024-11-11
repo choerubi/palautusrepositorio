03ab61358bf8833c8f3b02785f3f5c6e18c614de
choerubi <iida.porola@gmail.com>
2024-11-11 23:36:07 +0200

    Add week 2 task 12

--- week2/nhl-reader/src/index.py
+++ week2/nhl-reader/src/index.py
@@ -1,21 +1,43 @@
 import requests
 from player import Player
 
-def if_player_fin(player_dict):
-    return player_dict['nationality'] == "FIN"
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        
+        return players
+
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.player_reader = player_reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players = self.player_reader.get_players()
+
+        fin_players = []
+
+        for player in players:
+            if player.nationality == nationality:
+                fin_players.append(player)
+
+        fin_players.sort(reverse=True)
+        return fin_players
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
 
-    fin_players = filter(if_player_fin, response)
-    players = []
-
-    for player_dict in fin_players:
-        player = Player(player_dict)
-        players.append(player)
-
-    players.sort(reverse=True)
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     print("Players from FIN\n")
 
