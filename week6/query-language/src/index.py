24d8137321124dd1d76111da467903b71cad29da
choerubi <iida.porola@helsinki.fi>
2024-12-09 23:18:18 +0200

    Add week 6 task 4

--- week6/query-language/src/index.py
+++ week6/query-language/src/index.py
@@ -1,19 +1,21 @@
 from nhl_statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, PlaysIn, HasAtLeast, All, Not, HasFewerThan, Or
+from query_builder import QueryBuilder
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
+    query = QueryBuilder()
+
+    matcher = (
+        query
+        .plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
     )
 
     for player in stats.matches(matcher):
